fb8581627e58b72fb3233946b4ae88c19760214f
Jason Hughes <jasonhughes088@gmail.com>
2018-06-04 17:19:28 -0600

    add mikes insta

--- src/Media/Media.js
+++ src/Media/Media.js
@@ -5,17 +5,13 @@ import css from './Media.css';
 
 const Media = () => {
 
-
-
-
 return (
   <div>
     <div className='instagram-container'>
       <h2 className='instagram-text'>Recent Instagram Posts</h2>
       <script src="https://cdn.lightwidget.com/widgets/lightwidget.js"></script><iframe src="//lightwidget.com/widgets/88e916761aaf5d3bab9b8e3d4722152f.html" scrolling="no" allowtransparency="true" className="lightwidget-widget"></iframe>
     </div>
-      <script src="https://cdn.lightwidget.com/widgets/lightwidget.js"></script><iframe src="//lightwidget.com/widgets/88e916761aaf5d3bab9b8e3d4722152f.html" scrolling="no" allowtransparency="true" className="lightwidget-widget-mobile"></iframe>
-
+      <script src="https://cdn.lightwidget.com/widgets/lightwidget.js"></script><iframe src="//lightwidget.com/widgets/50cb8e2dc92453159ddf240a45b16bab.html" scrolling="no" allowtransparency="true" className="lightwidget-widget-mobile"></iframe>
   </div>
 )
 
